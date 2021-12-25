d9731a5ba21c0b9d50e24174c3f43c28d20396f7
GK <g.kula@chello.at>
2021-12-25 20:50:21 +0100

    Day 7 Part 1 & 2

--- 7.ps1
+++ 7.ps1
@@ -32,20 +32,80 @@ This costs a total of 37 fuel. This is the cheapest possible outcome; more expen
 Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?
 #>
 
-cd D:\Coding\Github\AdventofCode2021
+#cd D:\Coding\Github\AdventofCode2021
+cd /Users/georgkula/Projects/AdventofCode2021
 $data = (Get-Content ./input7.txt).Trim()
 $data = $data.Split(",") | Sort-Object
 
 $minPos = ($data | Measure-Object -Minimum).Minimum
 $maxPos = ($data | Measure-Object -Maximum).Maximum
-
+$lowestFuelCost = 0
 $maxShift = $maxPos - $minPos
 
 $newPos = 0
 $fuelCost = 0
 
-foreach ($pos in $data) {
-    $fuelcost += [Math]::Abs($newPos - $pos)
+for ($i = 0;$i -lt $maxPos;$i++) {
+    foreach ($pos in $data) {
+        $fuelcost += [Math]::Abs($i - $pos)
+    }
+    if ($lowestFuelCost -eq 0) {
+        $lowestFuelCost = $fuelCost
+    }
+    if ($lowestFuelCost -gt 0 -and $fuelCost -lt $lowestFuelCost) {
+        $lowestFuelCost = $fuelCost
+        $newPos = $i
+    }
+    $fuelCost = 0
 }
+$newPos
+$lowestFuelCost
 
-$fuelCost
+<#
+--- Part Two ---
+The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+
+As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+
+As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on; in the example above, this becomes 5:
+
+Move from 16 to 5: 66 fuel
+Move from 1 to 5: 10 fuel
+Move from 2 to 5: 6 fuel
+Move from 0 to 5: 15 fuel
+Move from 4 to 5: 1 fuel
+Move from 2 to 5: 6 fuel
+Move from 7 to 5: 3 fuel
+Move from 1 to 5: 10 fuel
+Move from 2 to 5: 6 fuel
+Move from 14 to 5: 45 fuel
+This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
+
+Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+#>
+
+
+$minPos = ($data | Measure-Object -Minimum).Minimum
+$maxPos = ($data | Measure-Object -Maximum).Maximum
+$lowestFuelCost = 0
+$maxShift = $maxPos - $minPos
+
+$newPos = 0
+$fuelCost = 0
+
+for ($i = 0;$i -lt $maxPos;$i++) {
+    foreach ($pos in $data) {
+        $steps = [Math]::Abs($i - $pos)
+        $fuelCost += ($steps/2) * ($steps + 1)
+    }
+    if ($lowestFuelCost -eq 0) {
+        $lowestFuelCost = $fuelCost
+    }
+    if ($lowestFuelCost -gt 0 -and $fuelCost -lt $lowestFuelCost) {
+        $lowestFuelCost = $fuelCost
+        $newPos = $i
+    }
+    $fuelCost = 0
+}
+$newPos
+$lowestFuelCost
